91f6eac30dc747f5e02097f4372aca13e242c182
viten1 <viten@ukr.net>
2023-03-11 18:25:49 +0200

    Add files via upload

---
+++ card_positions.py
@@ -0,0 +1,9 @@
+card_positions = {
+    1: '01_rytsar', 2: '02_vyzvanie', 3: '03_soroka', 4: '04_chayka', 5: '05_dom', 6: '06_buket',
+    7: '07_kvartira', 8: '08_luk', 9: '09_ryumka', 10: '10_mech', 11: '11_krest', 12: '12_koltso',
+    13: '13_rebenok', 14: '14_korabl', 15: '15_rytsar', 16: '16_doroga', 17: '17_pisma',
+    18: '18_dengi', 19: '19_zvony', 20: '20_serdce', 21: '21_dar', 22: '22_puteshestvie',
+    23: '23_myslitel', 24: '24_golova', 25: '25_dengi', 26: '26_kniga', 27: '27_puteshestvie',
+    28: '28_gorod', 29: '29_lisa', 30: '30_lotos', 31: '31_solntse', 32: '32_luna', 33: '33_klyuch',
+    34: '34_ryumka', 35: '35_korona', 36: '36_krysy'
+}
